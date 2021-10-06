b40b931cb20701df3a1c60af3da4690a90299431
paulogrillo <paulodigitalart@gmail.com>
2021-10-06 16:05:12 -0300

    Updated secutirity

---
+++ blog-spring/src/main/java/com/paulogrillo/blog/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.paulogrillo.blog.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.paulogrillo.blog.models.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	Optional<Usuario> findByUsuario(String usuario);
+}
