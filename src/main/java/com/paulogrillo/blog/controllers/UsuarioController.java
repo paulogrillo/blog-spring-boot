59acba616429754b9096654e06a832efeed7bccf
paulogrillo <paulodigitalart@gmail.com>
2021-11-14 04:30:41 -0300

    Updated endpoint user

--- src/main/java/com/paulogrillo/blog/controllers/UsuarioController.java
+++ src/main/java/com/paulogrillo/blog/controllers/UsuarioController.java
@@ -1,18 +1,23 @@
 package com.paulogrillo.blog.controllers;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.paulogrillo.blog.models.Usuario;
 import com.paulogrillo.blog.models.UsuarioLogin;
+import com.paulogrillo.blog.repository.UsuarioRepository;
 import com.paulogrillo.blog.service.UsuarioService;
 
 
@@ -22,7 +27,20 @@ import com.paulogrillo.blog.service.UsuarioService;
 public class UsuarioController {
 	
 	@Autowired
+	private UsuarioRepository repository;
+	
+	@Autowired
 	private UsuarioService usuarioService;
+	
+	@GetMapping
+	public ResponseEntity<List<Usuario>> GetAll() {
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> GetById(@PathVariable long id) {
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	}
 
 	@PostMapping("/logar")
 	public ResponseEntity<UsuarioLogin> Autentication(@RequestBody Optional<UsuarioLogin> user) {
@@ -34,5 +52,10 @@ public class UsuarioController {
 	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario) {
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(usuarioService.CadastrarUsuario(usuario));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Usuario> Put(@RequestBody Usuario usuario) {
+		return ResponseEntity.ok(repository.save(usuario));
 	}
 }
