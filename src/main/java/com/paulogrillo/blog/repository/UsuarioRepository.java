d9f5b1f2cb03bab2f05f9bf0a970a08ff20ce4ab
paulogrillo <paulodigitalart@gmail.com>
2021-11-22 17:54:42 -0300

    Fix

--- src/main/java/com/paulogrillo/blog/repository/UsuarioRepository.java
+++ src/main/java/com/paulogrillo/blog/repository/UsuarioRepository.java
@@ -4,10 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.paulogrillo.blog.models.Usuario;
 
-
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	
 	Optional<Usuario> findByUsuario(String usuario);
